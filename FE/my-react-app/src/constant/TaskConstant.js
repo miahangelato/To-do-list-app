07948dfd18272850a630948a99cca36c63e37986
miah4 <miah@simpleprojex.com>
2025-10-16 21:03:32 +0800

    feat:edited taskConstant

--- FE/my-react-app/src/constant/TaskConstant.js
+++ FE/my-react-app/src/constant/TaskConstant.js
@@ -1,6 +1,5 @@
 // Task API endpoints
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api'
-
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL 
 export const TASK_ENDPOINTS = {
   GET_ALL_TASKS: `${BASE_URL}/tasks`,
   GET_TASK_STATS: `${BASE_URL}/tasks/stats`,
